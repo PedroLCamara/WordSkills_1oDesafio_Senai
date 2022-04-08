54bafb679ace54548ee473d038ff8f00c65e99b3
PedroLCamara <plcrlschool@gmail.com>
2022-04-07 23:08:37 -0300

    ExercicioPAePG

--- API/WSTowers/WSTowers/Controllers/AlgoritmosController.cs
+++ API/WSTowers/WSTowers/Controllers/AlgoritmosController.cs
@@ -48,5 +48,73 @@ namespace WSTowers.Controllers
             }
             return Ok(NumerosPrimos);
         }
+
+        [HttpPost("DezNumerosSequenciados/")]
+        public IActionResult DezNumerosSequenciados(int[] listaNumeros)
+        {
+            if (listaNumeros.Length != 10)
+            {
+                return BadRequest(new { message = "A quantidade de números deve ser igual a 10!" });
+            }
+            bool PA = true;
+            bool PG = true;
+            int PosicaoNumeroPA = 1;
+            int PosicaoNumeroPG = 1;
+            int RazaoPA = listaNumeros[1] - listaNumeros[0];
+            int RazaoPG = listaNumeros[1] / listaNumeros[0];
+
+            //Logica verificacao PA
+            foreach (int numero in listaNumeros)
+            {
+                if (numero != (listaNumeros[0] + (PosicaoNumeroPA -1)*RazaoPA))
+                {
+                    PA = false;
+                    break;
+                }
+                PosicaoNumeroPA++;
+            }
+
+            //Logica Verificacao PG
+            foreach (int numero in listaNumeros)
+            {
+                int RazaoElevada = RazaoPG;
+                if ((PosicaoNumeroPG - 1) == 0)
+                {
+                    RazaoElevada = 1;
+                }
+                else
+                {
+                    for (int i = 1; i < (PosicaoNumeroPG - 1); i++)
+                    {
+                            int RazaoElevadaAnterior = RazaoElevada;
+                            RazaoElevada = RazaoElevadaAnterior * RazaoPG;
+                    }
+                }
+
+                if (numero != (listaNumeros[0] * RazaoElevada))
+                {
+                    PG = false;
+                    break;
+                }
+                PosicaoNumeroPG++;
+            }
+
+            if (PA == true && PG == true)
+            {
+                return Ok("É uma P.A e uma P.G!");
+            }
+            if (PA == true)
+            {
+                return Ok("É uma P.A!");
+            }
+            else if (PG == true)
+            {
+                return Ok("É uma P.G!");
+            }
+            else
+            {
+                return Ok("Definitivamente são números!");
+            }
+        }
     }
 }
